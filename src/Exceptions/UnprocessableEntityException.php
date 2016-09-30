084e962ba1488920a9a065961cf6b72ed3b25a43
James Kirkby <me@jameskirkby.com>
2016-09-30 13:31:56 +0100

    update 422 exception

--- src/Exceptions/UnprocessableEntityException.php
+++ src/Exceptions/UnprocessableEntityException.php
@@ -4,11 +4,13 @@ namespace Jkirkby91\Boilers\RestServerBoiler\Exceptions;
 
 /**
  * Class DeleteResourceFailedException
+ *
  * @package Jkirkby91\LumenRestServerComponent\Exceptions
+ * @author James Kirkby <jkirkby91@gmail.com>
  */
 class UnprocessableEntityException extends \Symfony\Component\HttpKernel\Exception\HttpException
 {
-    public function __construct($statusCode = 422, $message = 'Unprocessable Entity Exception', \Exception $previous = null, array $headers = array(), $code = 0)
+    public function __construct($statusCode = 422, $message = 'UnProcessable Entity Exception', \Exception $previous = null, array $headers = array(), $code = 0)
     {
         parent::__construct($statusCode,$message,$previous);
     }
