10dfa29cf5e27c81e57b59986689bc972332c614
James Kirkby <jkirkby@protonmailch>
2017-01-26 19:14:29 +0800

    allow custom messages to be sent to unauthed exception

--- src/Exceptions/UnauthorizedHttpException.php
+++ src/Exceptions/UnauthorizedHttpException.php
@@ -4,12 +4,14 @@ namespace Jkirkby91\Boilers\RestServerBoiler\Exceptions;
 
 /**
  * Class UnauthorizedHttpException
+ *
  * @package Jkirkby91\LumenRestServerComponent\Exceptions
+ * @author James Kirkby <jkirkby91@gmail.com>
  */
 class UnauthorizedHttpException	 extends \Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException
 {
-    public function __construct()
+    public function __construct($msg = 'Unauthorized Request')
     {
-        parent::__construct(null,'Unauthorized Request',null,401);
+        parent::__construct(null,$msg,null,401);
     }
 }
