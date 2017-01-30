9128167aa7744b29c5ee7506c3094dce55d847c0
James Kirkby <jkirkby@protonmailch>
2017-01-31 00:05:23 +0800

    syntax clean

--- src/Exceptions/PreconditionFailedHttpException.php
+++ src/Exceptions/PreconditionFailedHttpException.php
@@ -6,7 +6,7 @@ namespace Jkirkby91\Boilers\RestServerBoiler\Exceptions;
  * Class PreconditionFailedHttpException
  * @package Jkirkby91\LumenRestServerComponent\Exceptions
  */
-class PreconditionFailedHttpException	 extends \Symfony\Component\HttpKernel\Exception\PreconditionFailedHttpException
+class PreconditionFailedHttpException extends \Symfony\Component\HttpKernel\Exception\PreconditionFailedHttpException
 {
 
 }
